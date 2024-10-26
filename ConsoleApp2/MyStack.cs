a7bb4a74b81cd174b931090355f72944f1fa70d9
Ashot818 <ashot.h3.y@mail.ru>
2024-10-26 23:26:45 +0400

    first commit

---
+++ ConsoleApp2/MyStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class MyStack<T>
+    {
+        Queue<T> queue1 = new Queue<T>();
+        Queue<T> queue2 = new Queue<T>();
+
+        public void Push(T element)
+        {
+            queue2.Enqueue(element);
+
+            while (queue1.Count > 0)
+            {
+                queue2.Enqueue(queue1.Dequeue());
+            }
+
+            Queue<T> temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+        }
+        
+        public T Pop()
+        {
+           
+            if (queue1.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+                
+           return queue1.Dequeue();
+        }
+
+        public T Peek()
+        {
+
+            if (queue1.Count == 0)
+            {
+                throw new InvalidOperationException("stack is emtyp");
+            }
+            return queue1.Dequeue();
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            MyStack<int> stack = new MyStack<int>();
+            //Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            int x = stack.Pop();
+            
+            Console.WriteLine(x);
+
+        }
+    }
+}
